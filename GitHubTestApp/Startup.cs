920e218b20d401074dee77d3b3c59ebc1d743ac7
garda8 <erik.lovbom@studentconsulting.com>
2016-01-19 14:45:49 +0100

    laddat upp källkoden

---
+++ GitHubTestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubTestApp.Startup))]
+namespace GitHubTestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
